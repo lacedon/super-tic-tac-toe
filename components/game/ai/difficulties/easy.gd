4469fdd6dd3e2107cd1dfcf935ccb6f58d10bef0
Viktor Kukharev <lacedon1@gmail.com>
2023-10-28 16:16:14 -0400

    Refactor ai

--- components/game/ai/difficulties/easy.gd
+++ components/game/ai/difficulties/easy.gd
@@ -1,3 +1,8 @@
+# TODO: Refactor code to prefer edge cells if enemy stays in the center
+# 1 2 1
+# 2 x 2
+# 1 2 1
+
 const lines = [
 	{ start = 0, stepSize = 1 },
 	{ start = 0, stepSize = 3 },
@@ -12,7 +17,13 @@ const lines = [
 static func _getClosingCells(playerSign: TTT_State.PlayerSign, fieldList: Array[TTT_Cell_Resource]) -> Array[Array]:
 	var playerFieldType: = TTT_Cell_Resource.FieldType.x if playerSign == TTT_State.PlayerSign.x else TTT_Cell_Resource.FieldType.o
 	var enemyFieldType: = TTT_Cell_Resource.FieldType.o if playerSign == TTT_State.PlayerSign.x else TTT_Cell_Resource.FieldType.x
-	var result: Array[Array] = [[], [], [], [], []]
+	var result: Array[Array] = [
+		[], # Finish my 2-cell line
+		[], # Block enemy's 2-cell line
+		[], # Continue my 1-cell line
+		[], # Block enemy's 1-cell line
+		[], # Free cell
+	]
 
 	for line in lines:
 		var possiblePoints: Array[int] = []
@@ -28,21 +39,27 @@ static func _getClosingCells(playerSign: TTT_State.PlayerSign, fieldList: Array[
 					currentPlayerListItemNumber += 1
 				enemyFieldType:
 					enemyListItemNumber += 1
-				TTT_Cell_Resource.FieldType.none, TTT_Cell_Resource.FieldType.field:
+				TTT_Cell_Resource.FieldType.none:
 					possiblePoints.append(index)
+				TTT_Cell_Resource.FieldType.field:
+					if TTT_State_Selectors.getIsFieldFull(fieldList[index].inner):
+						possiblePoints.append(index)
 
-		if currentPlayerListItemNumber == 2: result[0].append_array(possiblePoints)
-		elif enemyListItemNumber == 2: result[1].append_array(possiblePoints)
-		elif currentPlayerListItemNumber == 1 && enemyListItemNumber == 0: result[2].append_array(possiblePoints)
-		elif enemyListItemNumber == 1: result[3].append_array(possiblePoints)
-		else: result[4].append_array(possiblePoints)
+		if currentPlayerListItemNumber == 2:
+			result[0].append_array(possiblePoints)
+		elif enemyListItemNumber == 2:
+			result[1].append_array(possiblePoints)
+		elif currentPlayerListItemNumber == 1:
+			result[2].append_array(possiblePoints)
+		elif enemyListItemNumber == 1:
+			result[3].append_array(possiblePoints)
+		else:
+			result[4].append_array(possiblePoints)
 
 	return result
 
 static func getCellsToChoose(state: TTT_State, openBlock: int, playerSign: TTT_State.PlayerSign) -> Array[int]:
 	var closingCellsList = _getClosingCells(playerSign, TTT_State_Selectors.getFieldList(state, openBlock))
-
-	if gameSettings.debug: _printDebugInfo(state, openBlock, closingCellsList)
 
 	var result: Array[int] = []
 	for closingCells in closingCellsList:
