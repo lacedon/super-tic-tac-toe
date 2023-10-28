409b118921d4e42ddd7b816aa8c17cc560c0154e
Viktor Kukharev <lacedon1@gmail.com>
2023-10-28 15:11:01 -0400

    Increase asset size

--- state/game-settings/ui-settings.gd
+++ state/game-settings/ui-settings.gd
@@ -2,7 +2,7 @@ extends Node
 
 var cellSize: int = ProjectSettings.get('display/window/size/viewport_width') / 3
 const gameFieldOffset: int = 32
-const gameFieldLineWidth: int = 8
+const gameFieldLineWidth: int = 16
 const playerXColor: Color = Color(0.8, 0.29, 0.09, 1) # cb4b16
 const playerOColor: Color = Color(0.15, 0.55, 0.82, 1) # 268bd2
 const lineColor: Color = Color(0.03, 0.21, 0.26, 1) # 073642
