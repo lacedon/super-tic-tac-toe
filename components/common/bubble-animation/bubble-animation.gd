4149680e6059aec1fa4f4702ad4d33686aba23a1
Viktor Kukharev <lacedon1@gmail.com>
2023-10-28 16:11:34 -0400

    Refactor animation component

--- components/common/bubble-animation/bubble-animation.gd
+++ components/common/bubble-animation/bubble-animation.gd
@@ -1,34 +1,36 @@
 extends Control
+class_name BubbleAnimation
 
-const Sign = preload('res://components/game/sign/sign.gd')
-const signNumber = 10
-const minSpeed = 30
-const maxSpeed = 100
-const stepMax = 20
+# const Sign = preload('res://components/game/sign/sign.gd')
+const itemNumber: int = 10
+const minSpeed: int = 30
+const maxSpeed: int = 100
+const stepMax: int = 100
 
 @export var opacity: float = 0.5
+@export var ItemCreator: Resource
 
-var signs = Array()
+var items: Array = Array()
 
 func _ready():
 	modulate.a = opacity
 
-	for index in range(signNumber):
-		var item: Sign = Sign.new()
-		item.value = TTT_State.FieldType.x if index % 2 == 0 else TTT_State.FieldType.o
+	for index in range(itemNumber):
+		var item: Node = ItemCreator.new()
+		item.value = TTT_Cell_Resource.FieldType.x if index % 2 == 0 else TTT_Cell_Resource.FieldType.o
 
 		item.position = Vector2(0, randf() * (size[1] - item.size[1]))
 
 		add_child(item)
 
-		signs.append({
+		items.append({
 			sign = item,
 			step = randf() * stepMax,
 			speed = randf() * (maxSpeed - minSpeed) + minSpeed
 		})
 
 func _process(delta):
-	for item in signs:
+	for item in items:
 		item.step += delta
 
 		var newX: float = (0.5 + 0.5 * sin(item.step)) * (size[0] - item.sign.size[0])
