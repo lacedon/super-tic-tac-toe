affa4d381dd3af32fb8498b48a0ac746f5a91831
Viktor Kukharev <lacedon1@gmail.com>
2023-11-03 22:10:14 -0400

    Fix margin

--- components/game/game-ui/game-ui.gd
+++ components/game/game-ui/game-ui.gd
@@ -3,7 +3,6 @@ extends Control
 @export var passStateNodes: Array[Node] = []
 @export var state: TTT_State:
 	set(value):
-		prints('passStateNodes', passStateNodes)
 		for node in passStateNodes:
 			if node:
 				node.state = value
