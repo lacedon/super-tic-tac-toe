e966975e40a7a0ca3df24ab9f997313b490bf555
Viktor Kukharev <lacedon1@gmail.com>
2023-11-20 20:08:41 -0500

    #23 Add saving config file

--- state/game-settings/game-settings.gd
+++ state/game-settings/game-settings.gd
@@ -11,6 +11,11 @@ enum GameMode {
 	hotSeat,
 }
 
+var _config: = ConfigFile.new()
+var _configFilePath: String = 'user://super-tic-tac-toe-config.cfg'
+var _configSection: String = 'gameSettingChanged'
+var _configPassword: String = 'super-tic-tac-toe'
+
 var mode: GameMode = GameMode.vsAI
 var aiDificulty: String = 'easy'
 var shouldShowFPS: bool = true
@@ -28,6 +33,8 @@ const lineColor: Color = Color(0.4, 0.48, 0.51, 1) # 667a82
 const lineColorActive: Color = Color(0.03, 0.21, 0.26, 1) # 073642
 
 func _ready():
+	_getConfigValue()
+
 	# Set background color
 	RenderingServer.set_default_clear_color(Color(0.99, 0.96, 0.89, 1))
 
@@ -35,3 +42,23 @@ func changeSetting(key: String, value: Variant):
 	var oldValue = self[key]
 	self[key] = value
 	emit_signal('gameSettingChanged', key, value, oldValue)
+
+	_config.set_value(_configSection, key, value)
+
+	var error = _config.save(_configFilePath)
+	if error != OK:
+		prints('WARN: Cannot save game setting. Error: ', error)
+
+func _getConfigValue():
+	var error = _config.load(_configFilePath)
+	if error != OK:
+		prints('WARN: Cannot load game setting. Error: ', error)
+		return
+
+	mode = _config.get_value(_configSection, 'mode', mode)
+	aiDificulty = _config.get_value(_configSection, 'aiDificulty', aiDificulty)
+	shouldShowFPS = _config.get_value(_configSection, 'shouldShowFPS', shouldShowFPS)
+	disableZoom = _config.get_value(_configSection, 'disableZoom', disableZoom)
+	masterVolume = _config.get_value(_configSection, 'masterVolume', masterVolume)
+	backgroundVolume = _config.get_value(_configSection, 'backgroundVolume', backgroundVolume)
+	uiVolume = _config.get_value(_configSection, 'uiVolume', uiVolume)
